303a3802e3ec410d2ab00d3680c34fc963305f7c
maamirkh <maamirkhan1983@gmail.com>
2024-03-25 23:33:21 +0500

    more guests added

---
+++ app.js
@@ -0,0 +1,12 @@
+#! usr/bin/env node
+let guests = ["Ahmed Ebrahim", "Danish Anwer", "Fahim Saleem",
+    "Sufyan Liaquat",];
+console.log("Great news! I found a bigger dinner table!");
+// Adding more guests
+guests.unshift("Zeeshan Kamal");
+guests.splice(guests.length / 2, 0, "Waqar Ansari");
+guests.push("Amin Khan");
+guests.forEach(guest => {
+    console.log(`Dear ${guest}, Would you like to join me for dinner? `);
+});
+export {};
